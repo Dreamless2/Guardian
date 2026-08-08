222e2080c2012e24ad94cc251be730d5684099f4
Dreamless2 <tiagonet50@gmail.com>
2026-08-08 20:10:57 -0300

    index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ async function main() {
 
             try {
                 const meta = await sock.groupMetadata(id)
-                
+                grouPName = meta.subject || 'unknown'
             }
 
             for (const p of participants) {
